36061b37f18f666f48f0cc9c1d35be2f4aa8b356
DESKTOP-B3A216I\Олег <puscas_oleg@mail.ru>
2020-09-07 14:00:49 +0300

    Message

---
+++ HomeWork2.16/src/HomeWork.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+        System.out.println("Enter two numbers");
+        System.out.println("Enter first number:");
+        Scanner input1 = new Scanner(System.in);
+        int number1 = input1.nextInt();
+        System.out.println("You entered: " + number1);
+        System.out.println("Enter second number:");
+        Scanner input2 = new Scanner(System.in);
+        int number2 = input2.nextInt();
+        System.out.println("You entered: " + number2);
+
+        if (number1 > number2) {
+            System.out.println("Number1 is larger than number2");
+        } else if (number2 > number1) {
+            System.out.println("Number2 is larger than number1");
+        } else {
+            System.out.println("These numbers are equal ");
+        }
+
+    }
+}
