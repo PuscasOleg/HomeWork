a9db0a74ab3b0e597bd42bdc617a386d586a3fbc
DESKTOP-B3A216I\Олег <puscas_oleg@mail.ru>
2020-09-07 14:38:00 +0300

    Message

---
+++ testmodule/src/Ex2_17.java
@@ -0,0 +1,28 @@
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.stream.*;
+
+public class Ex2_17 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[3];
+        System.out.println("Enter first number");
+        arr[0] = in.nextInt();
+        System.out.println("Enter second number");
+        arr[1] = in.nextInt();
+        System.out.println("Enter third number");
+        arr[2] = in.nextInt();
+        int sum = IntStream.of(arr).sum();
+        System.out.println("Sum:" + sum);
+        OptionalDouble average = IntStream.of(arr).average();
+        System.out.println("Average" + average);
+        OptionalInt max = IntStream.of(arr).max();
+        System.out.println("Max number" + max);
+        OptionalInt min = IntStream.of(arr).min();
+        System.out.println("Min number" + min);
+
+
+    }
+}
